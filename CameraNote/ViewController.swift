9a32d44339dab16ca11823cb263eaf674de6f1e0
hcai <hcai@twitter.com>
2014-12-29 21:50:28 -0500

    Initial Commit

---
+++ CameraNote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CameraNote
+//
+//  Created by Hongtao Cai on 12/29/14.
+//  Copyright (c) 2014 Hongtao Cai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
